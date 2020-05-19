2feb639d7104c40a9fade482ffc3734fe41d18ac
calderonra <00004017@uca.edu.sv>
2020-05-18 20:37:31 -0600

    Validaciones

--- src/main/java/com/uca/capas/demo/Domain/Estudiante.java
+++ src/main/java/com/uca/capas/demo/Domain/Estudiante.java
@@ -1,7 +1,11 @@
 package com.uca.capas.demo.Domain;
 
 
+
 import javax.persistence.*;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 
 
 @Entity
@@ -13,16 +17,24 @@ public class Estudiante {
     private Integer codigoEstudiante;
 
     @Column(name="nombre")
+    @NotEmpty()
+    @Size(message = "El nombre no debe tener mas de 50 de caracteres", max = 50)
     private String Nombre;
 
 
     @Column(name="apellido")
+    @Size(message = "El apellido no debe tener mas de 50 de caracteres", max = 50)
+    @NotEmpty()
     private String Apellido;
 
     @Column(name = "carne")
+    @Size(message = "El carne debe tener exactamente 10 caracteres", max = 10 , min = 10)
+    @NotEmpty()
     private String Carne;
 
     @Column(name = "carrera")
+    @Size(message = "La carrera no debe tener mas de 100 de caracteres", max = 100)
+    @NotEmpty()
     private String Carrera;
 
     public Estudiante() {
